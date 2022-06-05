3e5d9e19c3f699980e08e3ee131868f49e3922b8
Kamal Jain <kamaljain.ts@gmail.com>
2022-06-05 17:36:22 +0530

    Assignment-3 done

---
+++ Week-4/Assignment-3/index.js
@@ -0,0 +1,18 @@
+const Fib = (n) => ({
+  [Symbol.iterator]: () => {
+    let i = 1;
+    let old = 0,
+      new1 = 0;
+    return {
+      next: () => {
+        if (i++ <= n) {
+          [old, new1] = [new1, old + new1 || 1];
+          return { value: old, done: false };
+        } else {
+          return { done: true };
+        }
+      },
+    };
+  },
+});
+console.log([...Fib(7)]);
