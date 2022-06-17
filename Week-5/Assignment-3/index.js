9eeb74dc1c861f13de0777c659ad5509f3ab5cfc
Kamal Jain <kamaljain.ts@gmail.com>
2022-06-17 17:57:21 +0530

    Made code elaborative for easier understanding

--- Week-5/Assignment-3/index.js
+++ Week-5/Assignment-3/index.js
@@ -1,2 +1,8 @@
-const hasDuplicate = (arr) => new Set(arr).size !== arr.length;
-console.log(hasDuplicate([1, 1, -1, 4]));
+const getSetWithoutDuplicate = (arr) => {
+  return new Set(arr);
+};
+const hasDuplicate = (arr) => {
+  const lengthWithoutDuplicate = getSetWithoutDuplicate(arr).size;
+  return lengthWithoutDuplicate !== arr.length;
+};
+console.log(hasDuplicate([1, -1, 1, 2, 4]));
