1ca1e62afcf064ec00cf713e888a1726a1daa1b4
Kamal Jain <kamaljain.ts@gmail.com>
2022-06-17 17:28:40 +0530

    Added some more test cases

--- Week-5/Assignment-4/index.test.js
+++ Week-5/Assignment-4/index.test.js
@@ -1,13 +1,65 @@
 import mathOperations from "./index.js";
 
-test("testing addition operation", () => {
-  expect(mathOperations.sum(1, 2)).toBe(3);
+describe("testing addition operation", () => {
+  test("testing addition of 2 positive numbers", () => {
+    expect(mathOperations.sum(1, 2)).toBe(3);
+  });
+  test("testing addition of 1 positive number and 1 negative number", () => {
+    expect(mathOperations.sum(1, -9)).toBe(-8);
+  });
+  test("testing addition of 2 positive numbers", () => {
+    expect(mathOperations.sum(-1, -2)).toBe(-3);
+  });
+
+  test("testing addition of 2 float numbers", () => {
+    expect(mathOperations.sum(0.1, 0.2)).toBeCloseTo(0.3);
+  });
+  test("testing addition with 1 number only", () => {
+    expect(mathOperations.sum(1)).toBeFalsy();
+  });
+  test("testing addition without arguments", () => {
+    expect(mathOperations.sum()).toBeFalsy();
+  });
 });
 
-test("testing subtraction operation", () => {
-  expect(mathOperations.diff(1, 2)).toBe(-1);
+describe("testing subtraction operation", () => {
+  test("testing subtraction of 2 positive numbers", () => {
+    expect(mathOperations.diff(1, 2)).toBe(-1);
+  });
+  test("testing subtraction of 1 positive number and 1 negative number", () => {
+    expect(mathOperations.diff(1, -9)).toBe(10);
+  });
+  test("testing subtraction of 2 positive numbers", () => {
+    expect(mathOperations.diff(-1, -2)).toBe(1);
+  });
+  test("testing subtraction of 2 float numbers", () => {
+    expect(mathOperations.diff(0.1, 0.2)).toBeCloseTo(-0.1);
+  });
+  test("testing subtraction with 1 number only", () => {
+    expect(mathOperations.diff(1)).toBeFalsy();
+  });
+  test("testing subtraction without arguments", () => {
+    expect(mathOperations.diff()).toBeFalsy();
+  });
 });
 
-test("testing multiplication operation", () => {
-  expect(mathOperations.product(1, 2)).toBe(2);
+describe("testing multiplication operation", () => {
+  test("testing multiplication of 2 positive numbers", () => {
+    expect(mathOperations.product(1, 2)).toBe(2);
+  });
+  test("testing multiplication of 1 positive number and 1 negative number", () => {
+    expect(mathOperations.product(1, -9)).toBe(-9);
+  });
+  test("testing multiplication of 2 positive numbers", () => {
+    expect(mathOperations.product(-1, -2)).toBe(2);
+  });
+  test("testing multiplication of 2 float numbers", () => {
+    expect(mathOperations.product(0.1, 0.2)).toBeCloseTo(0.02);
+  });
+  test("testing multiplication with 1 number only", () => {
+    expect(mathOperations.product(1)).toBeFalsy();
+  });
+  test("testing multiplication without arguments", () => {
+    expect(mathOperations.product()).toBeFalsy();
+  });
 });
