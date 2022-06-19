8eb09227f43bf372962926eb004b16a62a98d188
Kamal Jain <kamaljain.ts@gmail.com>
2022-06-19 14:37:01 +0530

    Made Generator function consistent with Async Await function

--- Week-5/Assignment-1/index.js
+++ Week-5/Assignment-1/index.js
@@ -1,5 +1,5 @@
 async function asyncAwait() {
-  const task1 = await dotask1AA();
+  const task1 = await doTask1AA();
   console.log(task1);
   const task2 = await doTask2AA();
   console.log(task2);
@@ -7,7 +7,7 @@ async function asyncAwait() {
   console.log(task3);
 }
 
-async function dotask1AA() {
+async function doTask1AA() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const task1AA = await response.json();
   return task1AA;
@@ -27,17 +27,23 @@ async function doTask3AA() {
 
 // Using Generators:
 
-function generatorAsync() {
-  const task1 = doTask1();
-  task1.next().value.then((resp) => {
-    const task2 = doTask2();
-    task2.next().value.then((resp) => {
-      const task3 = doTask3();
-      task3.next().value.then((resp) => console.log(resp));
-      console.log(resp);
-    });
-    console.log(resp);
-  });
+async function generatorAsync() {
+  const task1 = await doTask1().next().value;
+  console.log(task1);
+  const task2 = await doTask2().next().value;
+  console.log(task2);
+  const task3 = await doTask3().next().value;
+  console.log(task3);
+  // const task1 = doTask1();
+  // task1.next().value.then((resp) => {
+  //   const task2 = doTask2();
+  //   task2.next().value.then((resp) => {
+  //     const task3 = doTask3();
+  //     task3.next().value.then((resp) => console.log(resp));
+  //     console.log(resp);
+  //   });
+  //   console.log(resp);
+  // });
 }
 
 function* doTask1() {
@@ -77,3 +83,4 @@ function* doTask3() {
 }
 
 generatorAsync();
+asyncAwait();
